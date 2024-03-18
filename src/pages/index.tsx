864ef777226b8da42e4ee429d3f86c0f4dcac87a
fahrezapratamh <fahrezaxgamer@gmail.com>
2024-03-18 21:18:05 +0700

    feat: restyling navbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,20 +2,38 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import { signIn, signOut, useSession } from "next-auth/react";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   const { data: session } = useSession();
   return (
     <>
-      <div className="mx-32 min-h-screen flex-1">
-        <div className="flex items-center justify-between border">
-          <h1>STORE</h1>
+      <div className="mx-32 min-h-screen">
+        <div className="flex items-center justify-between py-2">
+          <h1 className="font-bold tracking-wide">ZASTORE</h1>
           <div className="flex items-center gap-10 justify-between">
-            <h1>Home</h1>
-            <h1>Contact</h1>
-            <h1>product</h1>
-            <h1>Services</h1>
+            <Link href={"/"} className="font-semibold hover:text-primary">
+              Home
+            </Link>
+            <Link
+              href={"/contact"}
+              className="font-semibold hover:text-primary"
+            >
+              Contact
+            </Link>
+            <Link
+              href={"/product"}
+              className="font-semibold hover:text-primary"
+            >
+              products
+            </Link>
+            <Link
+              href={"/services"}
+              className="font-semibold hover:text-primary"
+            >
+              Services
+            </Link>
           </div>
           <div className="flex items-center ">
             <Button
